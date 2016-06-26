d0f7977c20150bd9416bc9e365214c568c1070c0
yamotech <nothings.2c9@gmail.com>
2016-06-26 14:31:42 +0900

    モデル設計

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :courses
+  resources :participants
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
